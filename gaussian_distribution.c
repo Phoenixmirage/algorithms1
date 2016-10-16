2c8b6541d42117a7a57694f84c82a06bfd3a3665
isministam <stamatelouismini@gmail.com>
2016-10-17 00:05:06 +0300

    changes

--- gaussian_distribution.c
+++ gaussian_distribution.c
@@ -1,25 +1,42 @@
 
 //Marsaglia Gaussian
-
-#include <cstdlib> //for rand max 
+#include <stdio.h>
+#include <stdlib.h> //for rand max 
 #include <math.h>
-#define RAND_MAX   //This value is implementation dependent. It's guaranteed that this value is at least 32767.
+#include <time.h>
+   //This value is implementation dependent. It's guaranteed that this value is at least 32767.
 
-int r,y1,y2,x1,x2;
-int M=-1;
-int N =1;
+float marsaglia(void);
 
-y1 = Μ + (rand() / RAND_MAX + 1.0)*(N-M+1);
-y2 = Μ + (rand() / RAND_MAX + 1.0)*(N-M+1);
-while((y1^2 + y2^2)>=1){
-	y1 = Μ + (rand() / RAND_MAX + 1.0)*(N-M+1);
-	y2 = Μ + (rand() / RAND_MAX + 1.0)*(N-M+1);
+int main(){
+	srand(time(NULL));
+	printf("lets choose 10 random numbers with gaussian distribution\n");
+	int i;
+	float j;
+	
+	for(i=1; i<=10; i++){
+		j=marsaglia();
+		printf("%dnumber: %f\n",i,j);
+	}
 }
 
-r = y1^2 + y2^2;
-x1=sqrt(-2*log(r^2)/(r^2))*y1;
-x2=sqrt(-2*log(r^2)/(r^2))*y2;			//δεν ξερω αν θες να κρατησουμε το 2ο χ
+float marsaglia(){
+float r,y1,y2,x1,x2;
+int M=-1;
+int N=1;
 
+y1 = M + (rand() /(RAND_MAX + 1.0)) * (N-M+1);
+y2 = M + (rand() /(RAND_MAX + 1.0)) * (N-M+1);
 
+while((pow(y1,2) + pow(y2,2))>=1){
+//	printf("%d\n%d\n",y1,y2);
+	y1 = M + (rand() /(RAND_MAX + 1.0)) * (N-M+1);
+	y2 = M + (rand() /(RAND_MAX + 1.0)) * (N-M+1);
+}
 
-//NORMAL : int r= N * (rand() / (RAND_MAX + 1.0)); οπου RAND_MAX σταθερη μεταβλητη που συνηθως ειναι ετοιμη
+r = pow(y1,2) + pow(y2,2);
+x1=sqrt(-2*log(pow(r,2))/(pow(r,2)))*y1;
+x2=sqrt(-2*log(pow(r,2))/(pow(r,2)))*y2;			
+
+return x1;
+}
